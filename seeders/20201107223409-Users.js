52530b5b5ae3f9d9443538d18ed5f5ff1914c454
Juan De Jesus <jdejesus93@gmail.com>
2020-11-07 18:40:42 -0400

    Generating seeders

---
+++ seeders/20201107223409-Users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const bcrypt = require("bcrypt")
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   const hashPassword = await bcrypt.hash('pass1234', 15)
+
+   await queryInterface.bulkInsert('Users', [{
+       firstName: "Test", 
+       lastName: "Administrator",
+       username: "administrator",
+       password: hashPassword
+      }], {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+};
